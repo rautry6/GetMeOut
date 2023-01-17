f532156092f99851ca3d00278d3f90951e9cf103
ironlionm4n <ironlionm4n@gmail.com>
2023-01-17 09:09:14 -0500

    Updated the scene with post processing and lights, updated player movement to account for coyote time and jump input buffer

--- Assets/PlayerRun.cs
+++ Assets/PlayerRun.cs
@@ -4,86 +4,118 @@ using UnityEngine;
 
 public class PlayerRun : MonoBehaviour
 {
-     [SerializeField] private Animator playerAnimator;
-        [SerializeField] private SpriteRenderer playerSpriteRenderer;
-        [SerializeField] private float moveSpeed = 1f;
-        [SerializeField] private Transform startPosition;
-        [SerializeField] private Transform groundCheck;
-        [SerializeField] private float groundCheckRayLength = 0.1f;
-        [SerializeField] private Rigidbody2D playerRigidbody;
-        [SerializeField] private float jumpForce;
-        [SerializeField] private LayerMask groundLayer;
-        private float _horizontal;
-        private bool _doubleJump;
-        private static readonly int HorizontalSpeed = Animator.StringToHash("horizontalSpeed");
-        public bool CanMove { get; set; } = true;
+    [Header("Assignable References")]
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Animator playerAnimator;
+    [SerializeField] private SpriteRenderer playerSpriteRenderer;
+    [SerializeField] private Rigidbody2D playerRigidbody;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
     
-        void Update()
+    [Header("Adjustable Variables")]
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float groundCheckRayLength = 0.1f;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpPressedRememberTime = 0.2f;
+    [SerializeField] private float groundedRememberTime = 0.2f;
+    [SerializeField] private float horizontalDamping;
+    
+    // Member Variables
+    private float _horizontal;
+    private bool _doubleJump;
+    private static readonly int HorizontalSpeed = Animator.StringToHash("horizontalSpeed"); // efficiency recommendation
+    private float _jumpPressedRemember = 0;
+    private float _groundedRemember = 0;
+    
+    // Properties
+    public bool CanMove { get; set; } = true;
+
+    void Update()
+    {
+        _jumpPressedRemember -= Time.deltaTime;
+        
+        if (!CanMove)
         {
-            if (!CanMove)
+            return;
+        }
+
+        if (IsGrounded())
+        {
+            _groundedRemember = groundedRememberTime;
+        }
+
+        // On the ground and no longer holding down the jump button be sure to set double jump back to false
+        if (IsGrounded() && !Input.GetButton("Jump"))
+        {
+            _doubleJump = false;
+        }
+        
+        // horizontal input and idle run animation logic
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        var playerCurrentHorizontalVelocity = playerRigidbody.velocity.x;
+        playerCurrentHorizontalVelocity += _horizontal;
+        playerCurrentHorizontalVelocity *= Mathf.Pow(1f - horizontalDamping, Time.deltaTime * 10f);
+        playerAnimator.SetFloat(HorizontalSpeed, Mathf.Abs(_horizontal));
+        HandleSpriteFlip(_horizontal);
+        
+        // first jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            _jumpPressedRemember = jumpPressedRememberTime;
+            // need to be on the ground or be jumping with the ability to double jump
+            if (_jumpPressedRemember > 0 && /*IsGrounded()*/ _groundedRemember > 0 || _doubleJump)
             {
-                return;
-            }
-    
-            if (IsGrounded() && !Input.GetButton("Jump"))
-            {
-                _doubleJump = false;
-            }
-            
-            _horizontal = Input.GetAxisRaw("Horizontal");
-            playerAnimator.SetFloat(HorizontalSpeed, Mathf.Abs(_horizontal));
-            HandleSpriteFlip(_horizontal);
-            if (Input.GetButtonDown("Jump"))
-            {
-                if (IsGrounded() || _doubleJump)
-                {
-                    playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
-                    _doubleJump = !_doubleJump;
-                }
-            }
-    
-            if (Input.GetButtonUp("Jump") && playerRigidbody.velocity.y > 0f)
-            {
-                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, playerRigidbody.velocity.y * 0.5f);
+                _jumpPressedRemember = 0f;
+                _groundedRemember = 0f;
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, jumpForce);
+                
+                // switch double jump to be the opposite of what it was to be sure to be able to double jump
+                _doubleJump = !_doubleJump;
             }
         }
     
-        private void FixedUpdate()
+        // if the players y velocity is above 0 then the player is in the middle of a jump and should double jump - need to introduce variable for the second jump power modifier
+        if (Input.GetButtonUp("Jump") && playerRigidbody.velocity.y > 0f)
         {
-            playerRigidbody.velocity =
-                new Vector2(_horizontal * moveSpeed * Time.fixedDeltaTime, playerRigidbody.velocity.y);
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, playerRigidbody.velocity.y * 0.5f);
         }
-    
-        private void HandleSpriteFlip(float horizontal)
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.velocity =
+            new Vector2(_horizontal * moveSpeed * Time.fixedDeltaTime, playerRigidbody.velocity.y);
+    }
+
+    private void HandleSpriteFlip(float horizontal)
+    {
+        if (horizontal < 0)
         {
-            if (horizontal < 0)
+            playerSpriteRenderer.flipX = true;
+        }
+        else if (horizontal > 0)
+        {
+            playerSpriteRenderer.flipX = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        var groundCheckHit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckRayLength, groundLayer);
+        if (groundCheckHit.collider != null)
+        {
+            var foundGround = groundCheckHit.collider.gameObject.GetComponent<Ground>();
+            if (foundGround != null)
             {
-                playerSpriteRenderer.flipX = true;
-            }
-            else if (horizontal > 0)
-            {
-                playerSpriteRenderer.flipX = false;
+                return true;
             }
         }
-    
-        private bool IsGrounded()
-        {
-            var groundCheckHit = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckRayLength, groundLayer);
-            if (groundCheckHit.collider != null)
-            {
-                var foundGround = groundCheckHit.collider.gameObject.GetComponent<Ground>();
-                Debug.Log(foundGround);
-                if (foundGround != null)
-                {
-                    return true;
-                }
-            }
-    
-            return false;
-        }
-    
-        public void ResetToStart()
-        {
-            transform.position = startPosition.position;
-        }
+
+        return false;
+    }
+
+    public void ResetToStart()
+    {
+        transform.position = startPosition.position;
+    }
 }
