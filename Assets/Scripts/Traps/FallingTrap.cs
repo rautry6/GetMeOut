b1f0f8b3971521e3067d40aa54b1ede85ad64cba
rautry6 <94204738+rautry6@users.noreply.github.com>
2023-02-25 12:48:12 -0500

    Floor Stick and Damage

--- Assets/Scripts/Traps/FallingTrap.cs
+++ Assets/Scripts/Traps/FallingTrap.cs
@@ -8,6 +8,7 @@ public class FallingTrap : MonoBehaviour
     [Header("Sway")]
     [SerializeField] private float swayRotation;
     [SerializeField] private float rotateAmount = 0.1f;
+    [SerializeField] private bool canSway = true;
     
     private float currentRotation = 0;
     private bool swayRight = true;
@@ -20,6 +21,9 @@ public class FallingTrap : MonoBehaviour
     [Header("Rigidbody")]
     [SerializeField] private Rigidbody2D trapRigidBody;
 
+    [Header("Damage"), Tooltip("True when the trap is hanging. False when it hits the ground so the player can walk through it.")]
+    [SerializeField] private bool canDamage = true;
+
 
     // Start is called before the first frame update
     void Awake()
@@ -30,7 +34,7 @@ public class FallingTrap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (swayRight)
+        if (swayRight && canSway)
         {
             currentRotation += rotateAmount;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
@@ -40,7 +44,7 @@ public class FallingTrap : MonoBehaviour
                 swayRight = false;
             }
         }
-        else
+        else if(canSway)
         {
             currentRotation -= rotateAmount;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, currentRotation);
@@ -54,6 +58,22 @@ public class FallingTrap : MonoBehaviour
         if (player.transform.position.x > transform.position.x - fallRange && player.transform.position.x < transform.position.x + fallRange)
         {
             trapRigidBody.gravityScale = 1;
+            canSway = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ground")
+        {
+            //Makes trap stick into floor
+            trapRigidBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            canDamage = false;
+        }
+
+        if(collision.tag == "Player" && canDamage)
+        {
+            player.GetComponent<PlayerHealth>().TakeDamage();
         }
     }
 }
